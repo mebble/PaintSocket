ba664ad66b70070812f874635143214503eb19d6
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2017-08-13 12:04:21 +0530

    setupe socket.io server

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var socket = require('socket.io');
 
 var app = express();
 var server = app.listen(3000);
 
 app.use(express.static('public'));
 console.log('My server is running');
+
+var io = socket(server);
+io.sockets.on('connection', function(socket) {
+	console.log(socket);
+});
