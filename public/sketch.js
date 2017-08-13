dc70ded8423ad0e368b2e66baaaa80256f0d49b8
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2017-08-13 12:21:24 +0530

    draw in mouseDragged, background in setup

--- public/sketch.js
+++ public/sketch.js
@@ -2,10 +2,18 @@ var socket;
 
 function setup() {
 	createCanvas(400, 400);
+	background('#000');
+
 	socket = io.connect('http://localhost:3000');
 }
 
 function draw() {
-	background('#000');
+
+}
+
+function mouseDragged() {
+	console.log(mouseX, mouseY);
+	noStroke();
+	fill('#fff');
 	ellipse(mouseX, mouseY, 40, 40);
 }
