debd2a4af77733921cace70373aacec1fdcfbfd6
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2017-08-13 12:49:52 +0530

    client receive mouse msg and draw

--- public/sketch.js
+++ public/sketch.js
@@ -5,16 +5,23 @@ function setup() {
 	background('#000');
 
 	socket = io.connect('http://localhost:3000');
+	socket.on('mouse', newDrawing);
 }
 
 function draw() {
 
 }
 
+function newDrawing(data) {
+	noStroke();
+	fill('#ff0000');
+	ellipse(data.x, data.y, 30, 30)
+}
+
 function mouseDragged() {
 	noStroke();
 	fill('#fff');
-	ellipse(mouseX, mouseY, 40, 40);
+	ellipse(mouseX, mouseY, 30, 30);
 
 	var data = {
 		x: mouseX,
